c321118c20753e500fc5a927c4f5a3ece96d4dc0
Zuzanna Cymerman <zuzannacymerman@Zuzannas-MacBook-Air.local>
2021-09-21 17:51:22 +0200

    Update layout

--- src/matryca_led_demo/WiFiConfig.java
+++ src/matryca_led_demo/WiFiConfig.java
@@ -4,6 +4,10 @@ import javax.swing.*;
 
 public class WiFiConfig {
     public JPanel wifi_config_panel;
+    private JTextField ssid_field;
+    private JTextField password_field;
+    private JComboBox pick_network;
+
     WiFiConfig(){
 
     }
